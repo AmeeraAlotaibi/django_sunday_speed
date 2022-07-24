24e0994916ccf35171e92129d529914500fba72b
AmeeraAlotaibi <meerareed.dev@outlook.com>
2022-07-24 16:40:10 +0300

    done

--- phones/models.py
+++ phones/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Phone(models.Model):
+    brand = models.CharField(max_length=30)
+    price = models.FloatField()
+    release = models.DateField()
+    cpu = models.CharField(max_length=25)
+
+    def __str__(self):
+        return Phone.brand
